d41702607d2111f871c62bbf7d1aafa6ed9697dc
Vastanonyymi <154027726+Vastanonyymi@users.noreply.github.com>
2023-12-17 08:01:01 +0200

    Update Vastalauta Post IDs.user.js
    Bugfixejä ja parannuksia

--- Vastalauta Post IDs.user.js
+++ Vastalauta Post IDs.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vastalauta Post IDs
 // @namespace    http://tampermonkey.net/
-// @version      1.1
+// @version      1.4
 // @description  Show post IDs next to the timestamps on posts
 // @author       Anonymous
 // @source       https://github.com/Vastanonyymi/vastalauta-userscript/
@@ -9,35 +9,78 @@
 // @grant        none
 // ==/UserScript==
 
-//tää laittaa viestien postausnumerot timestamppien viereen. jos ei toimi niin paina f5 ja toivo parasta tai sitte korjaa itte
+//Korjattu: urlit joiden perässä oli numero esim /b/123#1234 aiheutti scriptin toimimattomuuden
+//Korjattu: kataloginäkymään id:t myös
 
 (function() {
     'use strict';
 
     function insertPostIDs() {
-        const posts = document.querySelectorAll('.post');
+        const posts = document.querySelectorAll('.post, .js-post'); // Select both normal and catalog posts
 
         posts.forEach(post => {
             const postId = post.getAttribute('data-id');
-            const timestamp = post.querySelector('.post__timestamp');
+            if (postId) {
+                // Check if post ID span is already there to avoid duplicates
+                if (post.querySelector('.post-id')) return;
 
-            // Check if the ID is already displayed to avoid duplicates
-            if (!timestamp.nextSibling || timestamp.nextSibling.className !== 'post-id') {
                 const idSpan = document.createElement('span');
-                idSpan.textContent = `${postId}`;
-                idSpan.className = 'post-id'; // Add a class for potential styling via CSS
-                idSpan.style.cssText = `
-                    padding-top: 6px;
-                    margin-left: 0px; // Adjust spacing as needed
-                    font-size: inherit; // Match font size with the timestamp
-
-                `;
-
-                timestamp.insertAdjacentElement('afterend', idSpan);
+                idSpan.textContent = postId;
+                idSpan.className = 'post-id';
+                idSpan.style.marginRight = 'auto'; // Add space between ID and the menu button for catalog view
+                //idSpan.style.position = 'static';
+                idSpan.style.paddingTop = '6px';
+                // Determine where to insert the ID span
+                const menuButton = post.querySelector('.post__menu-button');
+                const timestamp = post.querySelector('.post__timestamp');
+                menuButton.style.position = 'static';
+                if (menuButton) { // Catalog view
+                    menuButton.parentNode.insertBefore(idSpan, menuButton);
+                } else if (timestamp) { // Normal view
+                    timestamp.insertAdjacentElement('afterend', idSpan);
+                }
             }
         });
     }
 
+    function handleURLChange() {
+        observer.disconnect(); // Disconnect observer during updates to avoid infinite loops
+        setTimeout(() => {
+            insertPostIDs();
+            observer.observe(bodyTarget, observerConfig); // Reconnect observer after updates
+        }, 500);
+    }
+
+    const observerConfig = { childList: true, subtree: true };
+    const observer = new MutationObserver(mutations => {
+        let shouldUpdate = false;
+        mutations.forEach(mutation => {
+            if (mutation.type === 'childList' && mutation.addedNodes.length) {
+                mutation.addedNodes.forEach(node => {
+                    if (!node.querySelector('.post-id')) {
+                        shouldUpdate = true;
+                    }
+                });
+            }
+        });
+        if (shouldUpdate) {
+            handleURLChange();
+        }
+    });
+
+    const bodyTarget = document.body;
+
+    if ('onurlchange' in window) {
+        window.addEventListener('urlchange', handleURLChange);
+    } else {
+        window.addEventListener('hashchange', handleURLChange);
+        window.addEventListener('load', handleURLChange);
+    }
+
+    // Initial execution and observer setup
+    insertPostIDs();
+    observer.observe(bodyTarget, observerConfig);
+})();
     // Run the function when the page loads and also when new posts are dynamically added if needed
     window.addEventListener('load', insertPostIDs);
 })();
